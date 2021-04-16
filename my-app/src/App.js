b682ad603371d12f5a6a7affc25f07a91f3579a8
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-04-16 10:17:21 +0600

    create class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./mycomponents/Hello";
+import Welcome from "./mycomponents/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Hello></Hello>
+      <Welcome></Welcome>
     </div>
   );
 }
