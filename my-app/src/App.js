3f002e792395cb89e2a8daf95d661443f15738bd
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-04-16 11:20:11 +0600

    onclick button used in functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,9 +7,6 @@ function App() {
   return (
     <div className="App">
       <Hello name="Saleh" age="20" living="Dhaka"></Hello>
-      <Hello name="Yousuf" age="25" living="Faridpur"></Hello>
-      <Hello name="Younus" age="30" living="Rajbari"></Hello>
-      <Hello name="Ibrahim" age="40" living="Kustia"></Hello>
       <Welcome name="React"></Welcome>
       <Welcome name="javascript"></Welcome>
       <Welcome name="Flexbox"></Welcome>
--- my-app/src/mycomponents/Hello.js
+++ my-app/src/mycomponents/Hello.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Hello(props) {
+  function doThis() {
+    alert("This is the button which is used in functional component");
+  }
   return (
     <div className="abc">
+      <button onClick={doThis}>Click Me</button>
       <h1>
         Hello! My name is {props.name}. I am {props.age} years old. I live in{" "}
         {props.living}.
