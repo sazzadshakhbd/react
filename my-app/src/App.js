507041309698e36cd744a6e60f3648a6e1830f85
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-04-16 10:54:35 +0600

    props defing using functoonal component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,10 @@ import Welcome from "./mycomponents/Welcome";
 function App() {
   return (
     <div className="App">
-      <Hello></Hello>
+      <Hello name="Saleh" age="20" living="Dhaka"></Hello>
+      <Hello name="Yousuf" age="25" living="Faridpur"></Hello>
+      <Hello name="Younus" age="30" living="Rajbari"></Hello>
+      <Hello name="Ibrahim" age="40" living="Kustia"></Hello>
       <Welcome></Welcome>
     </div>
   );
--- my-app/src/mycomponents/Hello.js
+++ my-app/src/mycomponents/Hello.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function Hello() {
+function Hello(props) {
   return (
     <div className="abc">
-      <h1>Hello! I am from Functional Component</h1>
+      <h1>
+        Hello! My name is {props.name}. I am {props.age} years old. I live in{" "}
+        {props.living}.
+      </h1>
     </div>
   );
 }
