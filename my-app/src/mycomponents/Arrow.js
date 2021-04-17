c5d6ed0d953ad297377e9d9e7995e35750dcf474
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-04-17 11:02:50 +0600

    Button functional using array function

--- my-app/src/mycomponents/Arrow.js
+++ my-app/src/mycomponents/Arrow.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+const ArrowBtn = (a) => {
+  return alert(a);
+};
+
 const Arrow = () => {
   return (
     <div>
-      <button className="btn btn-info m-5">Click Me</button>
+      <button
+        onClick={ArrowBtn.bind(this, "I am Arrow Function")}
+        className="btn btn-info m-5"
+      >
+        Click Me
+      </button>
     </div>
   );
 };
