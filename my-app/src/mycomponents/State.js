f2679083ffcab9867bd8958afb9b0608ce1f1739
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-04-18 22:37:34 +0600

    array declear using state

--- my-app/src/mycomponents/State.js
+++ my-app/src/mycomponents/State.js
@@ -3,18 +3,20 @@ import React, { Component } from "react";
 class State extends Component {
   constructor() {
     super();
-    let stateVar = {
-      name: "Sazzad",
+
+    this.state = {
+      name: {
+        member: ["Adib", "Adiba", "Marjia", "Maa"],
+      },
       age: "37",
       location: "Rajbari",
       gender: "Male",
     };
-    this.state = stateVar;
   }
   render() {
     return (
       <div>
-        <h1>My Name is {this.state.name}</h1>
+        <h1>My Name is {this.state.name.member[2]}</h1>
         <h1>I am {this.state.age} years old</h1>
         <h1>My Location is {this.state.location}</h1>
         <h1>I am {this.state.gender}</h1>
