c7af00d7d0ce3a562f3e139a3a36ca99f0af7368
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-04-29 15:50:29 +0600

    creating reactlist

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Select from "./myComponents/Select";
+import ReactList from "./myComponents/ReactList";
 
 function App() {
   return (
     <div className="App">
-      <Select />
+      <ReactList />
     </div>
   );
 }
