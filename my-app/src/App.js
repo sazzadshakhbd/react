99e75ae0ffe145be5c13d3fd807c93f2ccd73b66
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-04-30 17:23:41 +0600

    inline css add on app.js file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">home</div>;
+  return (
+    <div className="App">
+      <h1 style={{ backgroundColor: "teal", color: "white" }}>
+        Md. Sazzad Ali Shakh
+      </h1>
+    </div>
+  );
 }
 
 export default App;
