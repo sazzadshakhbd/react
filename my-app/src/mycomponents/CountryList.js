7ce72b70def4c3237cd031427b095d87fc1ddba5
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-05-02 10:37:23 +0600

    get menthod decleare using axios

--- my-app/src/mycomponents/CountryList.js
+++ my-app/src/mycomponents/CountryList.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 class CountryList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myData: [],
+    };
+  }
   componentDidMount() {
     axios
       .get("https://restcountries.eu/rest/v2/all")
       .then((response) => {
-        console.log(response.data);
+        this.setState({ myData: response.data });
       })
       .catch((error) => {
         console.log(error);
       });
   }
   render() {
-    return <div></div>;
+    const myList = this.state.myData;
+    const countryList = myList.map((x) => {
+      return <option>{x.name}</option>;
+    });
+
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="form-group">
+              <select className="form-control">{countryList}</select>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
