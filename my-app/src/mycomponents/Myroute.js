cc9ecb0033eddbdfa92d2a2f9317988834d628ad
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-05-02 17:38:58 +0600

    Not Found Page create

--- my-app/src/mycomponents/Myroute.js
+++ my-app/src/mycomponents/Myroute.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Portfolio from "../pages/Portfolio";
+import NotFound from "../pages/NotFound";
 
 class MyRoute extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/portfolio" component={Portfolio} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
