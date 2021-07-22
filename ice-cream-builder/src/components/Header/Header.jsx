cce5842345eab397fa2bee8c3f57808e5d017c9f
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-07-22 16:22:46 +0600

    create css and jsx file for header component

---
+++ ice-cream-builder/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+        <div className="container react" id="header">
+          <div>
+            <img className="logo" src="images/logo.svg" alt="Logo" />
+          </div>
+          <div className="textRight">
+            <img src="images/react.svg" alt="React" className="reactLogo" /><strong
+              >React</strong
+            >
+          </div>
+        </div>
+      </header>
+    )
+}
+
+export default Header
