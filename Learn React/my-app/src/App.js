3d24eefd32ce730e201c4255d90aee8c355a9a56
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-07-25 10:35:21 +0600

    How to create class component

--- Learn React/my-app/src/App.js
+++ Learn React/my-app/src/App.js
@@ -1,9 +1,11 @@
 import Hello from "./mycomponents/Hello";
+import Welcome from "./mycomponents/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Hello></Hello>
+      <Welcome></Welcome>
     </div>
   );
 }
