19a7e6c842ad63dbcc1305923a5de79bd6dcab56
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-07-25 12:11:58 +0600

    Passing arguments to event handlers using class component

--- Learn React/my-app/src/mycomponents/Welcome.jsx
+++ Learn React/my-app/src/mycomponents/Welcome.jsx
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 
 class Welcome extends Component {
-  myFunc() {
-    alert('Click me from Class Component')
+  myFunc(z) {
+    alert(z)
   }
   render() {
     return(
       <div>
-        <button onClick={this.myFunc}>Click me from Class Component</button>
+        <button onClick={this.myFunc.bind(this, 'I am class component')}>Click me Class</button>
       </div>
     )
   }
