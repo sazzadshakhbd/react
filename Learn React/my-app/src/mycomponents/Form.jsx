549adce71c9b8f018a78d5975f3424262ad1a874
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-07-31 14:49:16 +0600

    create signup form using onchange method

--- Learn React/my-app/src/mycomponents/Form.jsx
+++ Learn React/my-app/src/mycomponents/Form.jsx
@@ -5,35 +5,53 @@ class Form extends Component {
 
   constructor() {
     super()
-    this.state = {
-      username: ' '
+    this.state= {
+      firstName: ' ',
+      lastName: ' ',
+      email: ' ',
+      mobileNumber: ' '
     }
   }
  
-  changeText = (event) => {
+  onchangeHandelar = (event) => {
     var x = event.target.name;
     var y = event.target.value;
-    this.setState({[x]: y})
+    this.setState({[x]:y})
   }
+  
 
-  onSubmitHandler = () => {
-    alert(this.state.username)
-  }
+  
   render(){
     return(
       <div className="container">
-        <div className="row justify-content-center mt-5">
+        <div className="mt-5 row justify-content-center">
           <div className="col-md-6 bg-primary">
-            <form onSubmit={this.onSubmitHandler}>
-              <h1 className="py-3 text-center text-white">My First Form</h1>
-              <h2 className="text-center text-white">{this.state.username}</h2>
+            <form>
+              <h1 className="py-3 text-center text-white">Signup Form</h1>
+              
               <div className="form-group">
-                <input name="username" type="text" onChange={this.changeText} className="form-control" placeholder="Your name"></input>
+                <input onChange={this.onchangeHandelar} name="firstName" type="text" className="form-control" placeholder="First name"></input>
               </div>
-              <div className="form-group text-center">
+              <div className="form-group">
+                <input onChange={this.onchangeHandelar} name="lastName" type="text" className="form-control" placeholder="Last name"></input>
+              </div>
+              <div className="form-group">
+                <input onChange={this.onchangeHandelar} name="email" type="email" className="form-control" placeholder="Email"></input>
+              </div>
+              <div className="form-group">
+                <input onChange={this.onchangeHandelar} name="mobileNumber" type="number" className="form-control" placeholder="Mobile Number"></input>
+              </div>
+              <div className="text-center form-group">
                 <input type="submit" className="btn btn-large btn-outline-light" value="Submit Now"></input>
               </div>
             </form>
+          </div>
+          <div className="col-md-6 bg-primary">
+            <h1 className="py-3 text-center text-white">Form Text</h1>
+            <h3 className="mb-3 text-white">First Name: {this.state.firstName}</h3>
+            <h3 className="mb-3 text-white">Last Name: {this.state.lastName}</h3>
+            <h3 className="mb-3 text-white">Email Address: {this.state.email}</h3>
+            <h3 className="mb-3 text-white">Mobile Number: {this.state.mobileNumber}</h3>
           </div>
         </div>
       </div>
